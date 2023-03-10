a6c822d8d275b1ddbb0e96facee02eb8506318da
Anton Toropanov <at@level.travel>
2023-03-10 18:01:04 +0300

    Changes

--- src/core/HttpRequester.test.ts
+++ src/core/HttpRequester.test.ts
@@ -1,5 +1,44 @@
-const assert = require('assert');
+import {
+  useFakeXMLHttpRequest,
+  SinonFakeXMLHttpRequest,
+  SinonFakeXMLHttpRequestStatic
+} from 'sinon';
+import HttpRequester from './HttpRequester';
 
-it('should complete this test', function (done) {
-  // df
+describe('HttpRequester', () => {
+  let xhr: SinonFakeXMLHttpRequestStatic;
+  let instance: HttpRequester;
+  const requests: SinonFakeXMLHttpRequest[] = [];
+
+  beforeEach(() => {
+    xhr = useFakeXMLHttpRequest();
+
+    // @ts-ignore
+    global.XMLHttpRequest = xhr;
+
+    xhr.onCreate = ((request: SinonFakeXMLHttpRequest) => {
+      requests.push(request);
+    });
+
+    instance = new HttpRequester('/auth');
+  });
+
+  afterEach(() => {
+    requests.length = 0;
+  })
+
+  it('.get() should send GET request', () => {
+    instance.get('/user');
+
+    const [request] = requests;
+
+    expect(request.method).toBe('GET');
+  });
+  it('.post() should send POST request', () => {
+    instance.post('/logout');
+
+    const [request] = requests;
+
+    expect(request.method).toBe('POST');
+  });
 });
